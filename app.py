fdec5a3f9b3bc442066d50af79a1037ff92e7ca6
Felix Lutze <felix.lutze@outlook.de>
2021-04-10 16:17:51 +0200

    Test if the deployment works correctly

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "It's working"
+    return "Hello, this is my final 'Cloud DevOps Engineer Capstone Project'"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
