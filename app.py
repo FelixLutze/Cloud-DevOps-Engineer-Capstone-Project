e378247c70770e2265d58d7a3e9c6233d198f812
Felix Lutze <felix.lutze@outlook.de>
2021-04-10 21:22:36 +0200

    Fix Application

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1 style="text-align: center;">Welcome to my Cloud DevOps Engineer Capstone Project</h1>"
+    return "<h1 style='text-align: center;'>Welcome to my Cloud DevOps Engineer Capstone Project</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
