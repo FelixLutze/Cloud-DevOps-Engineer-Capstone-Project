ff9b632e2a00f977e737811ec22d4538a16cea95
Felix Lutze <felix.lutze@outlook.de>
2021-04-10 17:37:53 +0200

    Test deployment again

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, this is my final 'Cloud DevOps Engineer Capstone Project 2'"
+    return "Hello, this is my final 'Cloud DevOps Engineer Capstone Project'"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
